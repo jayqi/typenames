20af92b3a57f6caffeb7ee7d7484b0f0656596b1
Jay Qi <jayqi@users.noreply.github.com>
2025-04-18 23:51:16 -0400

    Rebuild docs for v1.0.0

---
+++ docs/main.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+def define_env(env):
+    @env.macro
+    def include_readme():
+        content = (Path(__file__).parent.parent / "README.md").read_text()
+        return content
+
+    @env.macro
+    def include_changelog():
+        content = (Path(__file__).parent.parent / "CHANGELOG.md").read_text()
+        return content
